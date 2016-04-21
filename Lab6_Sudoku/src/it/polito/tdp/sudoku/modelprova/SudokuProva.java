f509256240f0277698c96951593cd8807139b243
s205631 <s205631@studenti.polito.it>
2016-04-21 10:32:56 +0200

    Test prova 2

--- Lab6_Sudoku/src/it/polito/tdp/sudoku/modelprova/SudokuProva.java
+++ Lab6_Sudoku/src/it/polito/tdp/sudoku/modelprova/SudokuProva.java
@@ -11,18 +11,23 @@ public class SudokuProva {
 	public void SudokuProva(){
 		
 		solParziale=new ArrayList<Cella>();
+		for(int i=1;i<=9; i++){
+			for(int j=1;j<=9;j++){
+				solParziale.add(new Cella(i,j));
+			}
+		}
 	}
 	
 	public List<Cella> trova(){
 		
-		solParziale.clear();
+		//solParziale.clear();
 		
 		ricorsione(0);
 		
 		return solParziale;
 	}
 	
-	public boolean ricorsione(int livello){
+	private boolean ricorsione(int livello){
 		
 		if(livello==10){
 			return true;
@@ -44,7 +49,136 @@ public class SudokuProva {
 		return false;
 	}
 	
-	public boolean filtro(int livello,int col,List<Cella> solParziale,int val){
+	//MOSTRUOSO
+	private boolean controlloMatrice(int livello,int col,List<Cella> solParziale,int val){
+		//PRIMA "RIGA"
+		if(livello<=3){
+			//PRIMO QUADRATO
+			if(col<=3){
+				for(int i=0;i<=3;i++){
+					for(int j=0;j<=3;j++){
+						if(livello!=i && col!=j){
+							int p=solParziale.indexOf(new Cella(i,j));
+							if(solParziale.get(p).getValore()==val)
+								return false;
+						}
+					}
+				}
+			}
+			//TERZO QUADRATO
+			if(col>=7){
+				for(int i=0;i<=3;i++){
+					for(int j=7;j<=9;j++){
+						if(livello!=i && col!=j){
+							int p=solParziale.indexOf(new Cella(i,j));
+							if(solParziale.get(p).getValore()==val)
+								return false;
+						}
+					}
+				}
+			}else{
+				//SECONDO QUADRATO
+				for(int i=0;i<=3;i++){
+					for(int j=4;j<=6;j++){
+						if(livello!=i && col!=j){
+							int p=solParziale.indexOf(new Cella(i,j));
+							if(solParziale.get(p).getValore()==val)
+								return false;
+						}
+					}
+				}
+			}
+		}
+		//TERZA "RIGA"
+		if(livello>=7){
+			//PRIMO QUADRATO
+			if(col<=3){
+				for(int i=7;i<=9;i++){
+					for(int j=0;j<=3;j++){
+						if(livello!=i && col!=j){
+							int p=solParziale.indexOf(new Cella(i,j));
+							if(solParziale.get(p).getValore()==val)
+								return false;
+						}
+					}
+				}
+			}
+			//TERZO QUADRATO
+			if(col>=7){
+				for(int i=7;i<=9;i++){
+					for(int j=7;j<=9;j++){
+						if(livello!=i && col!=j){
+							int p=solParziale.indexOf(new Cella(i,j));
+							if(solParziale.get(p).getValore()==val)
+								return false;
+						}
+					}
+				}
+			}else{
+				//SECONDO QUADRATO
+				for(int i=7;i<=9;i++){
+					for(int j=4;j<=6;j++){
+						if(livello!=i && col!=j){
+							int p=solParziale.indexOf(new Cella(i,j));
+							if(solParziale.get(p).getValore()==val)
+								return false;
+						}
+					}
+				}
+			}
+		}else{
+			//SECONDA "RIGA"
+			
+			if(col<=3){
+				for(int i=4;i<=6;i++){
+					for(int j=0;j<=3;j++){
+						if(livello!=i && col!=j){
+							int p=solParziale.indexOf(new Cella(i,j));
+							if(solParziale.get(p).getValore()==val)
+								return false;
+						}
+					}
+				}
+			}
+			//TERZO QUADRATO
+			if(col>=7){
+				for(int i=4;i<=6;i++){
+					for(int j=7;j<=9;j++){
+						if(livello!=i && col!=j){
+							int p=solParziale.indexOf(new Cella(i,j));
+							if(solParziale.get(p).getValore()==val)
+								return false;
+						}
+					}
+				}
+			}else{
+				//SECONDO QUADRATO
+				for(int i=4;i<=6;i++){
+					for(int j=4;j<=6;j++){
+						if(livello!=i && col!=j){
+							int p=solParziale.indexOf(new Cella(i,j));
+							if(solParziale.get(p).getValore()==val)
+								return false;
+						}
+					}
+				}
+			}
+			
+		}
+		
+		return true;
+	}
+
+	
+	
+	private boolean filtro(int livello,int col,List<Cella> solParziale,int val){
+		
+		//CONTROLLO NUMERO GIA' INSERITO
+		
+		int temp=solParziale.indexOf(new Cella(livello,col));
+		if(solParziale.get(temp).getValore()!=0)
+			return false;
+		
 		
 		//CONTROLLO RIGA
 		for(int i=1;i<=9;i++){
@@ -67,9 +201,8 @@ public class SudokuProva {
 		}
 		
 		//CONTROLLO MATRICE
-		
-		
-		
+		if(controlloMatrice(livello,col,solParziale,val)== false)
+			return false;
 		
 		return true;
 	}
@@ -77,7 +210,9 @@ public class SudokuProva {
 	
 	
 	public static void main(String[] args) {
-	
+	SudokuProva sp=new SudokuProva();
+	List<Cella> l=sp.trova();
+	System.out.println(l);
 		
 		
 	}
